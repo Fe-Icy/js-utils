bd77601a3b2257a15d1e4a39bfc890d1b355bbaa
zhengyuanbing <zhengyuanbing@zhihu.com>
2019-07-21 01:24:09 +0800

    feat: auto README.md

--- 0.reader.js
+++ 0.reader.js
@@ -1,28 +1,38 @@
 const fs = require('fs')
 const path = require('path')
+const rm = require('rimraf')
 
-const title = '### 🚀✈️🚄 js 碎片化知识'
+const fileName = 'README.md'
+const staticContent = [
+	'### 🚀✈️🚄 js 碎片化知识\n\n'
+]
 const baseUrl = 'https://github.com/Fe-Icy/charming-javascript/blob/master/'
 
 const readFiles = p => {
 	const files = fs.readdirSync(p)
 	const fileUrls = files.filter(item => {
 		return !isNaN(item.slice(0, 1)) && Number(item.slice(0, 1)) !== 0
-	}).map(file => `[${file}](${baseUrl + file})\n`)
+	}).map(file => `[${file}](${baseUrl + encodeURIComponent(file)})`)
 	return fileUrls
 }
 
 const writeFile = (filename, data, filepath = __dirname) => {
-	data = data + '\n'
+	const lastData = data + '\n'
 	const lastPath = path.join(filepath, filename)
-  fs.writeFileSync(lastPath, data, function(err) {
+  fs.appendFileSync(lastPath, lastData, function(err) {
     if(err) {
       debug(`Error: some error occured, the status is ${err.status}`)
     }
   })
 }
-const allFiles = readFiles(__dirname)
 
-const fileContent = title + '\n' + '\n' + allFiles.join('')
 
-writeFile('README.md', fileContent)
+// 删除旧的 README.md
+rm(path.join(__dirname, fileName), () => {
+	const allFiles = readFiles(__dirname)
+	const allContent = [].concat(staticContent, allFiles)
+	allContent.forEach(file => {
+		writeFile(fileName, file)
+	})
+})
+
